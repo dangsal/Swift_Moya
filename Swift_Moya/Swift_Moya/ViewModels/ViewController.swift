e1f74bc5ce6c036d0a7e5aca4b92df945787eb51
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-06-13 15:55:10 +0900

    기본 세팅

---
+++ Swift_Moya/Swift_Moya/ViewModels/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Swift_Moya
+//
+//  Created by 이성호 on 2023/06/13.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - ui compoent
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupLayout()
+    }
+
+    // MARK: - func
+    
+    private func setupLayout() {
+        
+    }
+
+}
+
