4714cf0ec7abeb5bf87b75352e7ab06b458ed4f6
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-06-13 16:46:49 +0900

    모델 추가

---
+++ Swift_Moya/Swift_Moya/Models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  Swift_Moya
+//
+//  Created by 이성호 on 2023/06/13.
+//
+
+import Foundation
+
+struct Article {
+    let title: String?
+    let desription: String?
+}
---
+++ Swift_Moya/Swift_Moya/Models/ArticleList.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleList.swift
+//  Swift_Moya
+//
+//  Created by 이성호 on 2023/06/13.
+//
+
+import Foundation
+
+struct ArticleList {
+    let articles: [Article]
+}
+
+
